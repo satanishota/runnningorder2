02b2051dd9fdee20fc26e0b2628b8c775e127adf
satanishota <figuree5252@gmail.com>
2021-11-20 01:08:31 +0000

    1th

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :admins,:controllers => {
+
+  devise_for :users,:controllers => {
+ :registrations => 'public/registrations',
+ :sessions => 'public/sessions'}
+
+  devise_for :admins,path: 'admin',:controllers => {
  :registrations => 'admin/registrations',
  :sessions => 'admin/sessions'}
-  
+ 
+  namespace :admin do
+    get 'top' => 'homes#top'
+    root to: 'homes#top'
+  end
+
+
+ scope module: :public do
+ get 'top' => 'homes#top'
+ root to: 'homes#top'
+ get 'about' => 'homes#about'
+
+  end
 end
